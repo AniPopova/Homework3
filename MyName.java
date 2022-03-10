88d4d33e5a02e39e30b01f61d647fe2db164b932
AniPopova <100916508+AniPopova@users.noreply.github.com>
2022-03-10 14:09:01 +0200

    Try to share my homework
    All the tasks are put in here.

---
+++ MyName.java
@@ -0,0 +1,6 @@
+public class MyName {
+    public static void main(String[] args){
+        String fullName = "AnaPopova";
+        System.out.println(fullName.length() + " symbols are in my full name.");
+    }
+}
